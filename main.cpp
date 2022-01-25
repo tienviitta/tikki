ed4d4ce0effc844da18c4b8a7295e94cd0d548da
Petri Väisänen <petri.vaisanen@nordicsemi.no>
2022-01-25 09:58:46 +0200

    command line args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "cxxopts.hpp"
 #include "spdlog/fmt/ostr.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/stopwatch.h"
 
 #include <cstdlib>
 #include <ctime>
@@ -8,25 +9,43 @@
 #include <string>
 #include <vector>
 
-int main() {
+int main(int argc, const char **argv) {
     spdlog::info("Tikki card game:");
-    // Set global log level to debug
-    spdlog::set_level(spdlog::level::debug);
-    // Set rand seed
-    std::srand(0x123456);
-    // std::srand(std::time(nullptr));
-    std::vector<std::string> msg{"Hello", "C++",     "World",
-                                 "from",  "VS Code", "and the C++ extension!"};
-    std::vector<int> rands{};
-    for (const std::string &word : msg) {
-        int uni_rand = std::rand() % 100;
-        rands.emplace_back(uni_rand);
-        std::cout << word << " ";
+    spdlog::stopwatch sw;
+    // Command line arguments
+    cxxopts::Options options("Tikki", "Tikki card game");
+    // clang-format off
+    options.add_options()
+        ("d, debug", "Enable debugging")
+        ("p, players", "Number of players", cxxopts::value<int>()->default_value("2"))
+        ("h, help", "Print usage");
+    // clang-format on
+    options.allow_unrecognised_options();
+    auto result = options.parse(argc, argv);
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
     }
-    std::cout << std::endl;
+    bool debug = result["debug"].as<bool>();
+    int n_players = result["players"].as<int>();
+    // Set global log level to debug
+    if (debug) {
+        spdlog::set_level(spdlog::level::debug);
+        std::srand(0x123456);
+    } else {
+        std::srand(std::time(nullptr));
+    }
+    // Random integers
+    std::vector<int> rands{};
+    for (size_t i = 0; i < 30; i++) {
+        int uni_rand = std::rand() % (52 - i);
+        rands.emplace_back(uni_rand);
+    }
     std::stringstream ss_rands;
     std::copy(rands.begin(), rands.end(),
               std::ostream_iterator<int>(ss_rands, ", "));
-    spdlog::debug("  rands: [{}]",
-                  ss_rands.str().substr(0, ss_rands.str().length() - 2));
+    spdlog::info("  rands: [{}]",
+                 ss_rands.str().substr(0, ss_rands.str().length() - 2));
+    spdlog::debug("Elapsed {:.3} s", sw);
+    return 0;
 }
